78e16a202d7305e2cdfb07d4e2ea53703007a3c2
Katelynn <grayspence2@hotmail.com>
2023-12-16 16:50:59 -0500

    Added full 'addProperty' functionality to database.js

--- lightbnb-webapp/db/database.js
+++ lightbnb-webapp/db/database.js
@@ -126,7 +126,7 @@ const getAllReservations = function (guest_id, limit) {
 
   const getAllProperties = (options, limit) => {
     const queryParams = [];
-    
+
     // First half of query string... I.E: Before any modification by search function.
     let queryString = `
     SELECT properties.*, avg(property_reviews.rating) as average_rating
@@ -184,10 +184,49 @@ const getAllReservations = function (guest_id, limit) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  let queryString = `
+  INSERT INTO properties (
+    owner_id, 
+    title, 
+    description, 
+    thumbnail_photo_url, 
+    cover_photo_url, 
+    cost_per_night, 
+    street, 
+    city, 
+    province, 
+    post_code, 
+    country, 
+    parking_spaces, 
+    number_of_bathrooms, 
+    number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`
+
+  queryParams.push(
+    property.owner_id, 
+    property.title, 
+    property.description,
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code, 
+    property.country, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms);
+
+  return pool.query(queryString, queryParams) 
+  .then((results) => {
+    return results.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 module.exports = {
